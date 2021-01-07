f05ba45304b6f62e0424e0c60bb6d33a5d40a9f0
vicky patel <vickyspatel@gmail.com>
2021-01-07 20:56:25 +0530

    Created User Model

---
+++ lib/screens/home_material.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeMaterial extends StatefulWidget {
+  @override
+  _HomeMaterialState createState() => _HomeMaterialState();
+}
+
+class _HomeMaterialState extends State<HomeMaterial> {
+  @override
+  Widget build(BuildContext context) {}
+}
